3326991dffed1a52a8726a8d33d70cce468f537f
Weifeng1999 <ooiweifeng@outlook.com>
2019-10-30 21:58:31 +0800

    a

--- app/src/main/java/com/example/color/MainActivity.kt
+++ app/src/main/java/com/example/color/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
     }
 
     private fun makeColored(view: View) {
@@ -21,6 +24,13 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_six_text -> view.setBackgroundColor(Color.CYAN)
+            R.id.box_eleven_text -> view.setBackgroundColor(Color.BLACK)
+            R.id.box_twelve_text -> view.setBackgroundColor(Color.RED)
+            R.id.box_thirteen_text -> view.setBackgroundColor(Color.YELLOW)
+            R.id.box_fourteen_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_fifteen_text -> view.setBackgroundColor(Color.GREEN)
+            R.id.box_sixteen_text -> view.setBackgroundColor(Color.MAGENTA)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -32,8 +42,36 @@ class MainActivity : AppCompatActivity() {
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val boxSixText = findViewById<TextView>(R.id.box_six_text)
+        val boxElevenText = findViewById<TextView>(R.id.box_eleven_text)
+        val boxTwelveText = findViewById<TextView>(R.id.box_twelve_text)
+        val boxThirteenText = findViewById<TextView>(R.id.box_thirteen_text)
+        val boxFourteenText = findViewById<TextView>(R.id.box_fourteen_text)
+        val boxFifteenText = findViewById<TextView>(R.id.box_fifteen_text)
+        val boxSixteenText = findViewById<TextView>(R.id.box_sixteen_text)
+
+        val diceImage = findViewById<ImageView>(R.id.image_1)
+        diceImage.setImageResource(R.drawable.dice_1)
+
+        val diceImage2 = findViewById<ImageView>(R.id.image_2)
+        diceImage2.setImageResource(R.drawable.dice_2)
+
+        val diceImage3 = findViewById<ImageView>(R.id.image_3)
+        diceImage3.setImageResource(R.drawable.dice_3)
+
+        val diceImage4 = findViewById<ImageView>(R.id.image_4)
+        diceImage4.setImageResource(R.drawable.dice_4)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val clickableViews: List<View> =
+            listOf(boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, boxSixText, boxElevenText, boxTwelveText,
+                boxThirteenText, boxFourteenText, boxFifteenText, boxSixteenText, rootConstraintLayout)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
     }
 
 }
